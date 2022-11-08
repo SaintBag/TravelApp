c11360706384c4b172231df6bda87e799a8b7086
Sebastian Worek <sebastian1.worek@gmail.com>
2022-11-08 21:25:09 +0100

    init commit

---
+++ TravelApp/TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Sebastian on 08/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
